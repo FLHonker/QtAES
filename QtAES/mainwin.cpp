e33f2f8d5d8c55e29ff9d27f683f80ef49cd2f77
FLHonker <frankliuceo@163.com>
2017-10-27 13:07:50 +0800

    加密/解密OK，只是一次递归解密会乱码，多次就ok。

--- QtAES/mainwin.cpp
+++ QtAES/mainwin.cpp
@@ -2,12 +2,21 @@
 #include "ui_mainwin.h"
 #include <QByteArray>
 #include <QDebug>
+#include <QMessageBox>
 
 MainWin::MainWin(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWin)
 {
     ui->setupUi(this);
+    keyLen = 16;
+    blockSize = 16;
+    iMode = 0;
+    inputLen = 0;
+    memset(key, 0, sizeof(key)*4);
+    memset(szDataIn, 0, MAX_SIZE);
+    memset(szDataOut, 0, MAX_SIZE);
+    memset(szHex, 0, MAX_SIZE*2);
 }
 
 MainWin::~MainWin()
@@ -15,45 +24,75 @@ MainWin::~MainWin()
     delete ui;
 }
 
-// 从ui->comboBox获取key length转换为数值
-int MainWin::getkeyLen()
+// 将QString对象转换为char*
+void MainWin::QStr2Char(const QString qstr, char* newChar)
 {
-    int keyLen = 0;
-    int index = ui->comboBox_keyLen->currentIndex();  //获取comboBox中被选中的索引
-    switch(index)    //根据索引确定key length
-    {
-    case 0:
-        keyLen = 16;
-        break;
-    case 1:
-        keyLen = 24;
-        break;
-    case 2:
-        keyLen = 32;
-    }
-    return keyLen;
-}
-
-// 从ui->comboBox获取mode转换为数值
-int MainWin::getMode()
-{
-    int mode = ui->comboBox_mode->currentIndex();  //获取comboBox中被选中的索引,恰巧与mode值对应！
-
-    return mode;
-}
-
- // 将QString对象转换为char*
-char* MainWin::QStr2Char(const QString str)
-{
-    if(str.length()>1024*1024)
+    if(qstr.length()>1024*1024)
     {
         qDebug("QString长度超出栈大小（1MB）\n");
-        return NULL;
+        QMessageBox::warning(this,tr("Warning"),tr("The input-text is too large!"),QMessageBox::Yes);
+        return;
     }
-    QByteArray barray = str.toLatin1();
-    char *ch = barray.data();
+    /*
+    QByteArray barray = qstr.toLatin1();
+    newChar = (char*)malloc(qstr.length());
+    strcpy(newChar,barray.data());
+    */
+    std::string str = qstr.toStdString();
+    const char* ch = str.c_str();
+    if(sizeof(newChar)<sizeof(ch))
+    {
+        newChar = new char[qstr.length()];
+        memset(newChar, 0, sizeof(newChar)*4 - 1);
+    }
+    strcpy(newChar, ch);
+}
 
-    return ch;
+// 从ui->lineEdit获取Key值并互转化为char*
+void MainWin::getKey()
+{
+    QString keyStr = ui->lineEdit_key->text();
+    if(keyStr.isEmpty())
+    {
+        qDebug("Please input Key!");
+        QMessageBox::warning(this,tr("Warning"),tr("Please input Key!"));
+        return;
+    }
+    QStr2Char(keyStr, key);
+}
+
+// 从ui->textEdit获取文本并转化为char*；为szDataInszDataOut,szHex分配合适大小的内存空间;并把内容存入szDataIn。
+void MainWin::getInputText(char* szData)
+{
+    QString inputStr = ui->textEdit_input->document()->toPlainText();  //从textEdit控件中获取文本
+    QStr2Char(inputStr, szData);
+    inputLen = strlen(szDataIn);   //inputStr.length() + 1;
+
+    /*
+    szDataIn = new char[len];
+    memset(szDataIn,0,len-1);
+    szDataOut = new char[len];
+    memset(szDataOut,0,len-1);
+    szHex = new char[2*len];
+    memset(szHex,0,2*len-1);
+    strcpy(szDataIn, input);
+    */
+}
+
+//Function to convert unsigned char to string of length 2
+void MainWin::Char2Hex(unsigned char ch, char* szHex)
+{
+    char byte[2];
+    byte[0] = ch/16;
+    byte[1] = ch%16;
+    for(int i=0; i<2; i++)
+    {
+        if(byte[i] >= 0 && byte[i] <= 9)
+            szHex[i] = '0' + byte[i];
+        else
+            szHex[i] = 'A' + byte[i] - 10;
+    }
+    szHex[2] = 0;
 }
 
 //Function to convert string of length 2 to unsigned char
@@ -81,5 +120,113 @@ void MainWin::CharStr2HexStr(unsigned char const* pucCharStr, char* pszHexStr, i
     {
         Char2Hex(pucCharStr[i], szHex);
         strcat(pszHexStr, szHex);
+    }
+}
+
+//Function to convert string of chars to string of unsigned chars
+void MainWin::HexStr2CharStr(char const* pszHexStr, unsigned char* pucCharStr, int iSize)
+{
+    int i;
+    unsigned char ch;
+    for(i=0; i<iSize; i++)
+    {
+        Hex2Char(pszHexStr+2*i, ch);
+        pucCharStr[i] = ch;
+    }
+}
+
+int MainWin::hexcharToInt(char c)
+{
+    if (c >= '0' && c <= '9') return (c - '0');
+    if (c >= 'A' && c <= 'F') return (c - 'A' + 10);
+    if (c >= 'a' && c <= 'f') return (c - 'a' + 10);
+    return 0;
+}
+
+void MainWin::hexstringToBytes(char* hexstring,char* bytes,int hexlength)
+{
+    for (int i=0 ; i <hexlength ; i+=2)
+        *bytes++ = (char) ((hexcharToInt(hexstring[i]) << 4) | hexcharToInt(hexstring[i+1]));
+}
+
+// comboBox_keyLen中的选项触发的槽函数,保证数据块长度大于密钥；并设置key length
+void MainWin::on_comboBox_keyLen_currentIndexChanged(int index)
+{
+    ui->comboBox_blockLen->clear();    //首先清理一次项目
+    switch(index)
+    {
+        case 0:
+            ui->comboBox_blockLen->addItem(tr("16"));
+        case 1:
+            ui->comboBox_blockLen->addItem(tr("24"));
+        case 2:
+            ui->comboBox_blockLen->addItem(tr("32"));
+    }
+    keyLen = (index + 2)*8;    //设置key length
+}
+
+// 设置加密/解密mode,恰好等于索引
+void MainWin::on_comboBox_mode_currentIndexChanged(int index)
+{
+    iMode = index;
+}
+
+// 根据选中的内容设置blockSize
+void MainWin::on_comboBox_blockLen_currentTextChanged(const QString &arg1)
+{
+    blockSize = arg1.toInt();   //QString转换为int即可
+}
+
+// 清空input框，为新的输入做准备
+void MainWin::on_Btn_clear_clicked()
+{
+    ui->textEdit_input->clear();
+}
+
+// 加密
+void MainWin::on_Btn_encrypt_clicked()
+{
+    CRijndael m_Rijndael;         //AES实现对象
+    ui->textEdit_output->clear();       //清空output框
+    getInputText(szDataIn);
+    memset(szDataOut, 0, inputLen);
+    memset(szHex, 0, 2*inputLen);
+    qDebug()<<szDataIn<<strlen(szHex)<<" "<<blockSize<<" "<<strlen(szDataIn)<<endl;
+
+    try{
+        m_Rijndael.MakeKey(key,CRijndael::sm_chain0,keyLen,blockSize);
+        m_Rijndael.Encrypt(szDataIn,szDataOut,strlen(szDataIn),iMode);
+        //qDebug()<<"szDataOut:"<<szDataOut<<endl;
+
+        CharStr2HexStr((unsigned char*)szDataOut,szHex,strlen(szDataOut));
+        QString output(szHex);
+        ui->textEdit_output->setText(output);
+    } catch (QString exception) {
+        QMessageBox::warning(this,tr("Warning"),exception);
+    }
+}
+
+// 解密
+void MainWin::on_Btn_decrypt_clicked()
+{
+    CRijndael m_Rijndael;         //AES实现对象
+    ui->textEdit_output->clear();    //清空output框
+    memset(szHex, 0, MAX_SIZE * 2);
+    getInputText(szHex);
+    memset(szDataIn, 0, inputLen);
+    memset(szDataOut, 0, inputLen);
+    //hexstringToBytes(szHex, szDataIn, strlen(szHex));
+    HexStr2CharStr(szHex,(unsigned char*)szDataIn,strlen(szHex));   //将16进制转化为字符串
+    qDebug()<<szHex<<endl<<"Hex2Char:"<<szDataIn<<endl;
+
+    try{
+        m_Rijndael.MakeKey(key,CRijndael::sm_chain0,keyLen,blockSize);
+        m_Rijndael.Decrypt(szDataIn,szDataOut,strlen(szDataIn),iMode);
+        //m_Rijndael.Decrypt(szDataOut,szDataIn,strlen(szDataOut),iMode);
+
+        QString output(szDataOut);
+        ui->textEdit_output->setText(output);
+    } catch (QString exception) {
+        QMessageBox::warning(this,tr("Warning"),exception);
     }
 }
--- QtAES/mainwin.h
+++ QtAES/mainwin.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "Rijndael.h"
+#define MAX_SIZE 1024
 
 namespace Ui {
 class MainWin;
@@ -16,15 +17,36 @@ public:
     explicit MainWin(QWidget *parent = 0);
     ~MainWin();
 
+private slots:
+    void on_comboBox_keyLen_currentIndexChanged(int index);
+
+    void on_comboBox_mode_currentIndexChanged(int index);
+
+    void on_comboBox_blockLen_currentTextChanged(const QString &arg1);
+
+    void on_Btn_encrypt_clicked();
+
+    void on_Btn_decrypt_clicked();
+
+    void on_Btn_clear_clicked();
+
 private:
     Ui::MainWin *ui;
-    CRijndael m_Rijndael;    //AES实现对象
+    int keyLen;    //密钥块长度
+    int blockSize;   //数据块长度
+    int iMode;      //ECB,CBC,CFB模式索引
+    int inputLen;    //输入文本的长度
+    char key[50];    //key最长48个字符
+    char szDataIn[MAX_SIZE];    //存储输入文本的数组
+    char szDataOut[MAX_SIZE], szHex[MAX_SIZE * 2];
 
-    int getkeyLen();     //从ui->comboBox获取key length转换为数值
-    int getMode();       //从ui->comboBox获取mode转换为数值
-    char* QStr2Char(const QString str);    //将QString对象转换为char*
+    void getKey();      //从ui->lineEdit获取Key值并互赞华为char*
+    void QStr2Char(const QString qstr, char* newChar);    //将QString对象转换为char*
+    void getInputText(char* szData);   //从ui->textEdit获取文本并转化为char*
+    void Char2Hex(unsigned char ch, char* szHex);
     void Hex2Char(char const* szHex, unsigned char& rch);
     void CharStr2HexStr(unsigned char const* pucCharStr, char* pszHexStr, int iSize);
+    void HexStr2CharStr(char const* pszHexStr, unsigned char* pucCharStr, int iSize);
 };
 
 #endif // MAINWIN_H
